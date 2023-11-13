01f3513f8a42784686c2b7d1fa17999ccab7ec51
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-11-13 15:08:12 +0700

    Init AnimationAndroid project

---
+++ AnimationAndroid/app/src/main/java/com/nguyenhoangthanhan/animationandroid/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.nguyenhoangthanhan.animationandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nguyenhoangthanhan.animationandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initEvents()
+    }
+
+    private fun initEvents() {
+        binding.btnOpenDragDropItemRv.setOnClickListener {
+            startActivity(Intent())
+        }
+    }
+}
---
+++ AnimationAndroid/app/src/main/java/com/nguyenhoangthanhan/animationandroid/recycleview/dragdropitem/DragDropItemRVActivity.kt
@@ -0,0 +1,12 @@
+package com.nguyenhoangthanhan.animationandroid.recycleview.dragdropitem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nguyenhoangthanhan.animationandroid.R
+
+class DragDropItemRVActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drag_drop_item_rvactivity)
+    }
+}
